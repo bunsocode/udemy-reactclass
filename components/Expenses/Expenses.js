44d6ef17d7d7bd1b9535c36eb37c60e51fb2f8d7
JR Pascua <jrpascua@theoutcastagency.com>
2021-11-15 12:37:19 -0500

    added subfolders to components

--- components/Expenses/Expenses.js
+++ components/Expenses/Expenses.js
@@ -1,12 +1,13 @@
 import ExpenseItem from './ExpenseItem';
+import Card from '../UI/Card';
 import './Expenses.css';
 
 
 function Expenses(props){
     return (
-        <div class="expenses">
+        <Card className="expenses">
             <ExpenseItem 
-                title={props.items[0].title} 
+                title={props.items[0].title} git
                 amount={props.items[0].amount} 
                 date={props.items[0].date}
             />
@@ -25,7 +26,7 @@ function Expenses(props){
                 amount={props.items[3].amount} 
                 date={props.items[3].date}
             />
-        </div>
+        </Card>
     );
 }
 
